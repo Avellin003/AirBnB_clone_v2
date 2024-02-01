5fd3e33fd43351287288a4aaa76f5f2fbc9f9ef6
root <root@avellin.localdomain>
2024-02-01 16:48:13 +0200

    updates

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,34 +1,27 @@
 #!/usr/bin/env bash
-#web deployemtn of web_static
-
+# sets up servers for deplyoment
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
-sudo echo "Holberton School" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-printf %s "server {
-	listen 80 default_server;
-	listen [::] 80 default_server;
-	add_header X-serverd-By $HOSTNAME;
-	root /var/www/html;
-	index index.html index.htm;
-	location /hbnb_static {
-		alias /data/web_static/current;
-		index index.html index.htm;
-	}
-	location .redirect_me {
-		return 301 http://youtube.com/;
-	}
-	error_page 404 /404.html;
-	location /404 {
-		root /var/www/html;
-		internal;
-	}
-}" > /etc/nginx/sites-available/default
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
