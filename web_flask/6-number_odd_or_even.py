026456a37b31a6897afca1713064a5e8e1ab2c6d
root <root@avellin.localdomain>
2024-02-17 16:14:48 +0200

    updates

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Runs the web app"""
+from flask import Flask, render_template
+from urllib.parse import unquote
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Returns the Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Returns 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """Replace underscores with spaces"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def display_python(text='is cool'):
+    """Replace underscores with spaces"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """Displays the number"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_number_template(n):
+    """Checks whether it is an int"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def display_number_odd_or_even(n):
+    """Displays whether odd or even"""
+    return render_template('number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
