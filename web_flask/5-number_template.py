cce0bf6a245c77e8e8f45382b20ae114cbfd6607
root <root@avellin.localdomain>
2024-02-17 15:48:22 +0200

    updates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def display_number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number_template(n):
     """Checks whether it is an int"""
-    return render_template('5-number_template.html', number=n)
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == '__main__':
