af1e363a4469fec297b4e5c67b6ff2efca320ec1
root <root@avellin.localdomain>
2024-02-17 15:09:58 +0200

    updates

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""1-hbnb_route.py"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays the 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Returns 'HBNB'"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
